d362ebf0b000ec141a39fc3978ac123db5060cbc
Alex Gaudio <adgaudio@gmail.com>
2014-08-23 00:05:06 -0400

    readme + setup.py

---
+++ README.md
@@ -0,0 +1,14 @@
+This project aims to provide useful tools and algorithms for
+distributing Pandas objects on Spark.
+
+- Apache Spark is a fast and general engine for large-scale data
+processing.  It is written in Scala and also supports Python via
+PySpark.
+
+- Pandas is a library providing high-performance and easy-to-use data structures and data analysis tools for the Python programming language.
+
+
+```
+
+
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from distutils.core import setup
+    from setuptools import find_packages
+except ImportError:
+    print ("Please install Distutils and setuptools"
+           " before installing this package")
+    raise
+
+setup(
+    name='pyspark_pandas',
+    version='0.0.1-dev',
+    description=(
+        'Tools and algorithms for pandas Dataframes distributed on pyspark'),
+    long_description="Check the project homepage for details",
+    keywords=['spark pyspark pandas dataframe series', ],
+
+    author='Alex Gaudio',
+    author_email='adgaudio@gmail.com',
+    url='https://github.com/adgaudio/pyspark_pandas',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires = ['pandas'],
+    tests_require=['nose'],
+    test_suite="nose.main",
+)
