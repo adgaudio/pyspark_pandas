50959cf2e56b4c4b4e994401944f6fb973d58d70
Alex Gaudio <adgaudio@gmail.com>
2014-09-07 04:05:33 -0400

    add __version__

--- pyspark_pandas/__init__.py
+++ pyspark_pandas/__init__.py
@@ -1,4 +1,9 @@
-import logging
-log = logging.getLogger('pyspark_pandas')
+import logging as _logging
+log = _logging.getLogger('pyspark_pandas')
 
 from dataframe_rdd import DataFrameRDD
+
+import os.path as _p
+import pkg_resources as _pkg_resources
+__version__ = _pkg_resources.get_distribution(
+    _p.basename(_p.dirname(_p.abspath(__file__)))).version
